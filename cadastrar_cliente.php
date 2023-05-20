f70ee0ad8113ebf54e85d247ee349ce389e9e9bd
Maria Clara Monteiro <mcmpacheco7@gmail.com>
2023-05-20 15:21:21 -0300

    Adicionando validações aos campos do formulário da página cadastrar_cliente e inserindo conteúdo na tabela Clientes do BD

--- cadastrar_cliente.php
+++ cadastrar_cliente.php
@@ -1,3 +1,55 @@
+<?php
+include('conexao.php');
+
+if(count($_POST)>0){
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $nascimento = $_POST['nascimento'];
+    $erro = false;
+    if(empty($nome)){
+        $erro = "Preencha o nome.";
+    }
+    else if(empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $erro = "Preencha o email.";
+    } 
+  
+
+        if(!empty($nascimento)){
+            $pedacos = explode('/', $nascimento);
+                if(count($pedacos) == 3){  
+                    $nascimento = implode('-', array_reverse($pedacos));
+                } else{
+                    $erro = "A Data de Nascimento deve ter o padrão dia/mes/ano.";
+                }
+        }
+        if(!empty($telefone)){
+            function limparTexto($str){
+                return preg_replace("/[^0-9]/", "", $str);
+            }
+            $telefone = limparTexto($telefone);
+            
+            if(strlen($telefone)!= 11){
+                $erro = "Telefone inválido. O Telefone deve ser preenchido no padrão (22) 98888-8888";
+            }
+        }
+
+
+    if($erro){
+        echo "<b><p>ERRO: $erro</p></b>";
+    } else{
+        $sql_code = "INSERT INTO clientes(nome, email, telefone,nascimento, data) 
+        VALUES ('$nome', '$email', '$telefone', '$nascimento', NOW())";
+       $deucerto =  $mysqli -> query($sql_code) or die(mysqli_error($mysqli->error));
+        if($deucerto){
+            echo "<b><p>Cliente criado com sucesso.</p></b>";
+            unset($_POST);
+        } 
+    }
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -8,25 +60,25 @@
 </head>
 <body>
     <a href="/clientes.php">Voltar para a lista</a>
-    <form action="POST">
+    <form method="POST" action="">
         <p>
             <label>Nome </label>
-            <input name="nome" type="text">
+            <input name="nome" value="<?php if(isset($_POST['nome'])) echo $_POST['nome'];?>" type="text">
         <br>
         </p>
         <p>
             <label>Email </label>
-            <input name="email" type="text">
+            <input name="email" value="<?php if(isset($_POST['email'])) echo $_POST['email'];?>" type="text">
         <br>
         </p>
         <p>
             <label>Telefone </label>
-            <input name="telefone" type="text">
+            <input placeholder="(22) 9999-9999" value="<?php if(isset($_POST['telefone'])) echo $_POST['telefone'];?>" name="telefone" type="text">
         <br>
         </p>
         <p>
             <label>Data de Nascimento </label>
-            <input name="nascimento" type="date">
+            <input name="nascimento" value="<?php if(isset($_POST['nascimento'])) echo $_POST['nascimento'];?>" type="text">
         <br>
         </p>
         <p>
