2ac3597e2dd823a6c18a8369ccb78b55324950c3
Maria Clara Monteiro <mcmpacheco7@gmail.com>
2023-05-20 14:12:48 -0300

    Adicionando formulario de cadastro de cliente a página de cadastar_clientes

--- cadastrar_cliente.php
+++ cadastrar_cliente.php
@@ -7,6 +7,31 @@
     <title>Cadastrar Cliente</title>
 </head>
 <body>
-    <label for=""></label>
+    <a href="/clientes.php">Voltar para a lista</a>
+    <form action="POST">
+        <p>
+            <label>Nome </label>
+            <input name="nome" type="text">
+        <br>
+        </p>
+        <p>
+            <label>Email </label>
+            <input name="email" type="text">
+        <br>
+        </p>
+        <p>
+            <label>Telefone </label>
+            <input name="telefone" type="text">
+        <br>
+        </p>
+        <p>
+            <label>Data de Nascimento </label>
+            <input name="nascimento" type="date">
+        <br>
+        </p>
+        <p>
+            <button type="submit">Salvar </button>
+        </p>
+    </form>
 </body>
 </html>
